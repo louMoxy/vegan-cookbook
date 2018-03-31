241969f40d4165fdb52329206d1550c4357babd4
unknown <louisemoxy@gmail.com>
2018-03-31 10:16:31 +0100

    Add tester recipe page and styling

---
+++ src/templates/recipe-post.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { kebabCase } from 'lodash'
+import Helmet from 'react-helmet'
+import Link from 'gatsby-link'
+import Content, { HTMLContent } from '../components/Content'
+require('../style/recipe.scss');
+
+export const RecipePostTemplate = ({
+  contentComponent,
+  tags,
+  title,
+  helmet,
+  method,
+  ingredients,
+  prepTime,
+  cookTime,
+  serves,
+  images
+}) => {
+  const PostContent = contentComponent || Content;
+  return (
+    <section className="recipe">
+      {helmet || ''}
+      <h1 className="recipe__title">
+        {title}
+      </h1>
+      <div className="recipe__container">
+        <h3>List of Ingredients</h3>
+          <div className="recipe__ingredients">
+            {ingredients.map( (ingredient, index) => (
+              <p key={index}>{ingredient}</p>
+            ))}
+            {ingredients.length % 2 !==0 ? <p></p> : null}
+          </div>
+          <h3>Method</h3>
+          <div className="recipe__method">
+            <PostContent content={method} />
+          </div>
+          <div className="recipe__info">
+            <p><span>Cook Time: </span>{cookTime}</p>
+            <p><span>Serves: </span>{serves}</p>
+            <p><span>Prep Time: </span>{prepTime}</p>
+            <p></p>
+          </div>
+      </div>
+            {/* {tags && tags.length ? (
+              <div>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map(tag => (
+                    <li key={tag + `tag`}>
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null} */}
+        <div className="recipe__image">
+          {images.map( (img, index) => (
+              <div key={index} className={img.imgSize}>
+                <img src={img.image} alt={img.alt || 'Food image'}/>
+              </div>
+            ))}
+        </div>
+    </section>
+  )
+}
+
+RecipePostTemplate.propTypes = {
+  contentComponent: PropTypes.func,
+  title: PropTypes.string,
+  helmet: PropTypes.instanceOf(Helmet),
+  tags: PropTypes.array,
+  method: PropTypes.string,
+  ingredients: PropTypes.array,
+  prepTime: PropTypes.string,
+  cookTime: PropTypes.string,
+  serves: PropTypes.string,
+  images: PropTypes.array
+}
+
+const BlogPost = ({ data }) => {
+  const { markdownRemark: post } = data
+  return (
+    <RecipePostTemplate
+      contentComponent={HTMLContent}
+      helmet={<Helmet title={`${post.frontmatter.title} | Recipe`} />}
+      tags={post.frontmatter.tags}
+      title={post.frontmatter.title}
+      method={post.html}
+      ingredients={post.frontmatter.ingredients}
+      prepTime={post.frontmatter.prepTime}
+      cookTime={post.frontmatter.cookTime}
+      serves={post.frontmatter.serves}
+      images={post.frontmatter.images}
+    />
+  )
+}
+
+BlogPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
+
+export default BlogPost
+
+export const pageQuery = graphql`
+  query RecipePostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        title
+        tags
+        ingredients
+        prepTime
+        cookTime
+        serves
+        images {
+          image
+          imgSize
+      	}
+      }
+    }
+  }
+`
