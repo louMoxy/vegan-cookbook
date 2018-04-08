2a3004202e4445b685479ede532646f5ca9440ad
unknown <louisemoxy@gmail.com>
2018-04-08 16:39:46 +0100

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+require('../style/footer.scss');
+
+const Footer = () => (
+  <footer>
+      <p>Copyright © 2018 <a href="http://louisemoxhay.co.uk/" target="_blank">Louise Moxhay</a></p>
+  </footer>
+)
+
+export default Footer
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-
-import Navbar from '../components/Navbar'
-import '../style/root.scss'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import '../style/root.scss';
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Gatsby + Netlify CMS" />
     <Navbar />
-    <div className="content">{children()}</div>
+    <div className="content">{children()}
+    <Footer />
+    </div>
   </div>
 )
 
